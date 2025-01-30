f915e618618d7d9ac84b8f7d0936b3580fa8bf85
Haruaki Tomida <h.tomida@neogenia.co.jp>
2025-01-30 15:50:30 +0900

    feat: prototypeパターン

---
+++ src/c06_prototype/Prototype.cs
@@ -0,0 +1,102 @@
+namespace C06_Prototype {
+  public class GoodsManager {
+    private IDictionary<string, Goods> prototypes = new Dictionary<string, Goods>();
+    public void Register(string identifier, Goods goods) {
+      this.prototypes[identifier] = goods;
+    }
+    public Goods Create(string identifier) {
+      return this.prototypes[identifier].CreateClone();
+    }
+  }
+
+  public interface Goods {
+    public string? Identifier{get; set;}
+    public Image? PrintingImage{get; set;}
+    public Goods CreateClone();
+    public Recipe GetProductRecipe();
+  }
+
+  public sealed class Image {
+    // 何等かの画像データを保持するクラス
+  }
+  
+  public class Recipe {
+    // 商品の製造方法を保持するクラス
+  }
+
+  public class AcrylGoods : Goods {
+    public string? Identifier{get; set;}
+    public Image? PrintingImage{get; set;}
+    public Size3D? Size{get; set;}
+    public Shape? Shape{get; set;}
+    public Recipe GetProductRecipe() {
+      var rp = new Recipe();
+      // TODO: アクリル製品の製造方法をrpに設定して返す
+      return rp;
+    }
+    public Goods CreateClone() {
+      return new AcrylGoods {
+        Identifier = this.Identifier,
+        PrintingImage = this.PrintingImage,
+        Size = this.Size,
+        Shape = this.Shape
+      };
+    }
+  }
+
+  public class Sticker : Goods {
+    public string? Identifier{get; set;}
+    public Image? PrintingImage{get; set;}
+    public Material? Material{get; set;}
+    public List<Shape> CutLines{get; set;} = new List<Shape>();
+    public Recipe GetProductRecipe() {
+      var rp = new Recipe();
+      // TODO: ステッカーの製造方法をrpに設定して返す
+      return rp;
+    }
+    public Goods CreateClone() {
+      return new Sticker {
+        Identifier = this.Identifier,
+        PrintingImage = this.PrintingImage,
+        Material = this.Material,
+        CutLines = this.CutLines
+      };
+    }
+  }
+  public class Size3D {
+    public double Width{get; set;}
+    public double Height{get; set;}
+    public double Depth{get; set;}
+  }
+
+  public sealed class Shape {
+    // 何等かの形状データを保持するクラス
+  }
+
+  public sealed class Material {
+    // シールの材質等のデータを保持するクラス
+  }
+
+  public class Main {
+    public static void Run() {
+      var manager = new GoodsManager();
+      var acrylGoods = new AcrylGoods {
+        Identifier = "acryl001",
+        PrintingImage = new Image(),
+        Size = new Size3D { Width = 10, Height = 20, Depth = 5 },
+        Shape = new Shape()
+      };
+      var sticker = new Sticker {
+        Identifier = "sticker001",
+        PrintingImage = new Image(),
+        Material = new Material(),
+        CutLines = new List<Shape> { new Shape(), new Shape() }
+      };
+      // 同じ種類のプロダクトでも寸歩や形状が異なるものをそれぞれプロトタイプとして登録する
+      manager.Register("acryl001", acrylGoods);
+      manager.Register("sticker001", sticker);
+      var acrylClone = manager.Create("acryl001");
+      var stickerClone = manager.Create("sticker001");
+    }
+  }
+}
---
+++ src/c06_prototype/README.md
@@ -0,0 +1,54 @@
+# Prototypeパターン
+
+## 概要
+
+模型となるインスタンスから別のインスタンスを複製して使用する
+
+## 詳細
+
+TemplateMethodパターン等と同じで、何かオブジェクトを作成する際に｢元となるなにか｣がクラスやインターフェースではなくインスタンス(オブジェクト)になる。
+模型となるインスタンスから適切に複製を作るために `cleateClone`というメソッドを実装させるようにする
+Javaではこのような複製が作れるよ、ということを表すインターフェースが仕組みとして備わっている(`java.lang.Cloneable`)
+
+## 実装例
+
+オリジナルグッズ販売管理システム
+
+任意の画像が印刷されたアクリルキーホルダーやステッカーなどを管理する。
+原型となるそれぞれのグッズのクラス(アクキークラス、ラバキークラス)があり、それぞれ形状を整えた(丸型だったり、星型だったり)プロトタイプをManagerに登録し、製造時にプロトタイプとして原型からCloneしてくることを想定する。
+
+```mermaid
+classDiagram
+
+class GoodsManager {
+  -Dictionary<Goods> prototypes
+  +Register(Goods)
+  +Create(String Identifier)
+}
+
+class Goods {
+<<interface>>
+  +string Identifier
+  +Image PrintingImage
+  +CreateClone()*
+  +GetProductRecipe()*
+}
+
+class AcrylGoods {
+  +Size3D : 3辺の寸法
+  +Shape : 切り抜きの形
+  +CelateClone()
+  +GetProductRecipe()
+}
+
+class Sticker {
+  +Material : シールの材質タイプ
+  +CutLines : シールのカットラインのシェイプ
+  +CelateClone()
+  +GetProductRecipe()
+}
+
+AcrylGoods --|> Goods : Extends
+Sticker --|> Goods : Extends
+GoodsManager --> Goods : Uses
+```
